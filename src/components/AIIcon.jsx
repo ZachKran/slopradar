3186d9bb8116428a74728b7b16696dd9b7fef1e2
ZachKran <zachkranabetter@gmail.com>
2026-09-05 16:47:39 -0400

    increase sizes

--- src/components/AIIcon.jsx
+++
@@ -1,3 +0,0 @@
-export default function AIIcon({ size = 24 }) {
-  return <img src="/icons/ai-icon.png" alt="AI slop" width={size} height={size} style={{ display: "block", objectFit: "contain" }} draggable={false} />;
-}
--- src/components/RealIcon.jsx
+++
@@ -1,3 +0,0 @@
-export default function RealIcon({ size = 24 }) {
-  return <img src="/icons/real-icon.png" alt="Real photo" width={size} height={size} style={{ display: "block", objectFit: "contain" }} draggable={false} />;
-}
